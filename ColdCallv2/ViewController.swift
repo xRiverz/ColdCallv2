737659ff889c704811a7b52f0ba63e50a1f94932
xRiverz <anhargalam@gmail.com>
2021-12-05 11:57:43 +0000

    .

--- ColdCallv2/ViewController.swift
+++ ColdCallv2/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var names: UILabel!
+    let name : [String] = [ "Anhar" , "Ahme" , "Rahaf" , "Khaled" , "Rwan" , "Yaser" ]
+    @IBOutlet weak var numbers : UILabel!
+    @IBAction func coldcallPressed(_ sender: UIButton) {
+        let no = Int.random(in: 1...5)
+        names.text = name.randomElement()!
+        numbers.text = String(no)
+        if (no < 3){
+            numbers.textColor = UIColor.red
+        }
+        else if (no < 5 && no > 2 ){
+            numbers.textColor = UIColor.orange
+        }
+        else{
+            numbers.textColor = UIColor.green        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        names.text = "Click Me"
+        numbers.text = ""
     }
 
 
